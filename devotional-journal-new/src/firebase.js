b0f31af4ed38630790325d177ce0ba8fb72073e3
T8020Token <anthony@t8020consulting.com>
2025-05-08 18:04:47 +0000

    Remove Firebase Analytics to fix CSP violation

--- devotional-journal-new/src/firebase.js
+++ devotional-journal-new/src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,6 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-export { auth, db, analytics };
+export { auth, db };
